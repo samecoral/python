34ad85ff6dc0cb84945f8cccb9ec31c988d4eed8
caoheshan <caoheshan1010@163.com>
2019-11-20 23:52:43 +0800

    学习类

--- LearnClass.py
+++ LearnClass.py
@@ -2,18 +2,16 @@ class LearnClass(object):
     a, b, c = '1', 2, ["zhangsan", "lisi", "王五"]
 
     def fun1(self):
-        print(' 我是谁，我是'+LearnClass.a)
-
-
+        print(' 我是谁，我是' + LearnClass.a)
 
     @classmethod
-    def fun2(cls,age,b):
-        print('类方法取值'+cls.a+'获取外部值'+str(age))
+    def fun2(cls, age, b):
+        print('类方法取值' + cls.a + '获取外部值' + str(age))
         cls.b = b
 
 
 LearnClass.fun1(LearnClass)
-LearnClass.fun2(18,99)
+LearnClass.fun2(18, 99)
 print(LearnClass.b)
 LearnClass.a = '10'
 LearnClass.fun1(1)
@@ -21,9 +19,9 @@ LearnClass.fun1(1)
 
 class cl_b(object):
     var1 = '1'
+
     def fun(self):
         return self.var1
-
 
 
 Class1 = cl_b()
@@ -31,3 +29,14 @@ Class1.var1 = 10
 print(Class1.var1)
 Class2 = cl_b()
 print(Class2.var1)
+
+
+class ClassB(object):
+    var1 = 1
+
+    def fun(self):
+        return self.var1
+
+
+b = ClassB()
+print(b.fun())
